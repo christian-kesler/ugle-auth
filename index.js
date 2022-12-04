5db0118ce869a01e7811019f81dfe9b5109009eb
Chris <christian.j.kesler@gmail.com>
2022-12-04 12:00:20 -0700

    Contribute towards additional methods

--- index.js
+++ index.js
@@ -15,36 +15,32 @@ function parameters
     dtb - variable representing sqlite database connection
     args - object containing varying information necessary for each function
         create_fields - string containing the database fields to write to
-            (email, hash, created_at, created_by)
+            "email, hash, created_at, created_by"
         create_params - the values to be written into the database
-            email
-            password
+            email - variable
+            password - variable
 
         read_fields - string containing the database fields to retrieve
-            id
-            email
-            created_at
-            created_by
+            "id, email, created_at, created_by"
         read_key - variable containing the field to index
-            id
-            email
-            created_at
-            created_by
+            (id || email || created_at || created_by)
         read_value - variable containing the value to search for
-            
+            ${varies}
+
         update_fields - string containing the database fields to modify
-            (email, hash)
+            "email, hash"
         update_params - the values to be written into the database
-            email
-            hash
+            data - variable containing the actual content
+            hash - boolean defining whether the data should be hashed before storing (PASSWORDS WILL NOT WORK UNLESS HASHED)
         update_key - variable containing the field to index
-            (id, email)
+            (id || email)
         update_value - variable containing the value to search for
+            ${varies}
 
         delete_key - variable containing the field to index
-            (id, email)
+            (id || email)
         delete_value - variable containing the value to search for
-
+            ${varies}
     callback - executed upon completion of the package function
         err - FALSE if successful, object if function failed
             message - descriptive string of what went wrong, taken from sqlite when possible
@@ -142,44 +138,109 @@ module.exports = {
         // TODO: testing
         await tryCreateTable(dtb)
 
-        if (!validEmail(args.create_params.email)) {
-            callback(
-                {
-                    message: "invalid email"
-                }
-            )
-        } else {
-            if (!validPassword(args.create_params.password)) {
+        try {
+            if (!validEmail(args.create_params.email)) {
                 callback(
                     {
-                        message: "invalid password"
+                        message: "invalid email"
                     }
                 )
             } else {
-                await dtb.exec(`INSERT INTO auth(${args.create_fields}) VALUES(?, ?);`, [
-                    args.create_params.email,
-                    hash(args.create_params.password)
-                ], (err) => {
-                    if (err) {
-                        callback(
-                            {
-                                message: err.message
-                            }
-                        )
-                    } else {
-                        callback(
-                            false
-                        )
-                    }
-                })
+                if (!validPassword(args.create_params.password)) {
+                    callback(
+                        {
+                            message: "invalid password"
+                        }
+                    )
+                } else {
+                    await dtb.exec(`INSERT INTO auth(${args.create_fields}) VALUES(?, ?);`, [
+                        args.create_params.email,
+                        hash(args.create_params.password)
+                    ], (err) => {
+                        if (err) {
+                            callback(
+                                {
+                                    message: err.message
+                                }
+                            )
+                        } else {
+                            callback(
+                                false
+                            )
+                        }
+                    })
+                }
             }
+        } catch (err) {
+            callback(
+                {
+                    message: err.message
+                }
+            )
         }
     },
     readUser: async (dtb, args, callback) => {
-        // TODO: programming
+        // TODO: testing
+        await tryCreateTable(dtb)
+
+        try {
+            await dtb.all(`SELECT ${args.read_fields} FROM auth WHERE ${args.read_key} = ${args.read_value};`, [], (err, rows) => {
+                if (err) {
+                    callback(
+                        {
+                            message: err.message
+                        },
+                        null
+                    )
+                } else {
+                    callback(
+                        false
+                    ),
+                        rows
+                }
+            })
+        } catch (err) {
+            callback(
+                {
+                    message: err.message
+                },
+                null
+            )
+        }
     },
     updateUser: async (dtb, args, callback) => {
-        // TODO: programming
+        // TODO: testing
+        await tryCreateTable(dtb)
+
+        try {
+            if (args.update_params.hash == true) {
+                args.update_params.data = hash(args.update_params.data)
+            }
+            await dtb.exec(`UPDATE auth SET ? = ? WHERE ? = ?;`, [
+                args.update_field,
+                args.update_params.data,
+                args.update_key,
+                args.update_value
+            ], (err) => {
+                if (err) {
+                    callback(
+                        {
+                            message: err.message
+                        }
+                    )
+                } else {
+                    callback(
+                        false
+                    )
+                }
+            })
+        } catch (err) {
+            callback(
+                {
+                    message: err.message
+                }
+            )
+        }
     },
     deleteUser: async (dtb, args, callback) => {
         // TODO: programming
